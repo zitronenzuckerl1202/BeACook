ee83150a3743d3800df115c08c5960b3af366845
christina <chrisiby4@gmail.com>
2022-12-15 11:55:41 +0100

    #7; detail view - button back to menu

--- frontend/src/components/recipe/recipe-table-component.ts
+++ frontend/src/components/recipe/recipe-table-component.ts
@@ -4,6 +4,7 @@ import { Recipe } from "../../model/recipe"
 import recipeService from "../../recipe-service"
 const tableTemplate = html`
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <h3>Recipe Table<h3>
     <table class="w3-table-all w3-monospace">
         <thead>
             <tr>
@@ -36,7 +37,6 @@ class RecipeTableComponent extends HTMLElement{
         this.recipes = await this.load()
         this.render(this.recipes)
     }
-    
     private render(recipes: [Recipe]){
         render(tableTemplate, this.shadowRoot)
 
